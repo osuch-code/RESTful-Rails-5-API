9d679ce9b14c694137e3913e4663b321f0165303
Ted Osuch <ted@osuch.com>
2019-11-11 13:56:14 +0000

    added superhero faker and api docs link

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-# Check it in
 
 ActiveRecord::Schema.define(version: 2019_11_11_060121) do
 
@@ -19,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_11_11_060121) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.string "superhero"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-5.times do
+50.times do
   Article.create({
       title: Faker::Book.title,
-      body: Faker::Lorem.sentence
+      body: Faker::Lorem.sentence,
+      superhero: Faker::Superhero.name
   })
 end
